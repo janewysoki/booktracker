607da60501bacb53b490b7a2ff694d33f00b91e6
janewysoki <jane.wysoki@gmail.com>
2020-02-10 14:14:55 -0500

    Add route and create form to add new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,13 @@
 class BooksController < ApplicationController
+    #get books/new to render a form to create new entry
+    get '/books/new' do #get reqeusts show something exists
+        erb :'/books/new'
+    end
+    #post books to create a new book
+    post '/books' do
+        
+    end
+    #show route for a book
+
+    #index route for all books
 end
