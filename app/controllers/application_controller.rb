62117fc82a0655ed2a301b3ec0190cb6f8513a74
janewysoki <jane.wysoki@gmail.com>
2020-02-09 21:10:25 -0500

    Create login route in UsersController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,14 +14,4 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
-
-  helpers do
-    def current_user
-      @user ||=User.find(session[:user_id])
-    end
-
-    def logged_in?
-      !!session[:user_id]
-    end
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    #what routes do i need for login?
+    #purpose of this route is to render login page/form
+    get '/login' do
+        erb :'users/login'
+    end
+
+    #what routes do i need for signup?
+    get '/signup' do
+    end
 end
