adda5d3a716b596b3f6c99eddbdddf446661a6f6
janewysoki <jane.wysoki@gmail.com>
2020-02-09 19:23:39 -0500

    Revise users migration table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,27 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+  use Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :sessions_secret, 'secrets'
+    set :sessions_secret, 'booklover'
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+    def current_user
+      @user ||=User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
 end
