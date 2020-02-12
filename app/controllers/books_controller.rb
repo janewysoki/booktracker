074ad81d3728f3cee50983f56a6449d54bda1b94
janewysoki <jane.wysoki@gmail.com>
2020-02-11 23:33:31 -0500

    Add find_book method to BooksController to eliminate repetition

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,8 @@ class BooksController < ApplicationController
     #show route for a book; show/render particular book entry
     get '/books/:id' do #dynamic route - dynamic pieces of the route (:id) becomes key value pairs in the parameters
         #find book entry
-        @book = Book.find(params[:id])
+        #@book = Book.find(params[:id])
+        find_book
         #redirects destroy instance variables, and we want this instance variable to live on and be available inside show page for books
         erb :'/books/show'
     end
@@ -37,9 +38,31 @@ class BooksController < ApplicationController
     #this route should send us to books/edit.erb which will render an edit form
     get '/books/:id/edit' do
         #want to find a specific book before rendering the edit form
-        @book = Book.find(params[:id]) #have to pull the id from the url
+        #@book = Book.find(params[:id]) #have to pull the id from the url
+        find_book
         erb :'/books/edit'
     end
 
+    #this action's job is 
+    #this is a brand new action, i don't have access to the instance variable anymore
+    patch '/books/:id' do
+        #find the book
+        #@book = Book.find(params[:id])
+        find_book
+        #modify (update) the book
+        #redirect to show page
+
+    end
+
     #index route for all books
+
+    # i see that this line of code: @book = Book.find(params[:id]) is used multiple times so i should create a helper method
+    private #WHY IS THIS PRIVATE?
+        def find_book
+            @book = Book.find(params[:id])
+        end
+
 end
+
+
+
