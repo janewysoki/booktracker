b22cf405af215dfdeb19a96236612362716a82d3
janewysoki <jane.wysoki@gmail.com>
2020-02-16 19:19:35 -0500

    Delete unnecessary line of code from login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password]) 
             session[:user_id] = @user.id 
-            #puts session
             flash[:message] = "Welcome, #{@user.username}!" 
             redirect "users/#{@user.id}" 
         else
